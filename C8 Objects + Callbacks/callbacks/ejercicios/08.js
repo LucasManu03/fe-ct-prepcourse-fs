4789cf8ff6823c6f7fadb3676069e704a0657dd3
LucasManu <luquif03@gmail.com>
2024-04-04 20:31:22 -0300

    checkpoint

--- C8 Objects + Callbacks/callbacks/ejercicios/08.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/08.js
@@ -3,6 +3,16 @@ const buscarElemento = (array, callback) => {
   // Si el elemento no se encuentra, devuelve el mensje "No se encontró el elemento".
   // La función de callback es la encargada de evaluar si el elemento fue encontrado.
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    // Verificar si el callback devuelve true para el elemento actual
+    if (callback(array[i])) {
+      // Si el elemento cumple con la condición, devolverlo
+      return array[i];
+    }
+  }
+
+  // Si no se encuentra ningún elemento que cumpla con la condición, devolver el mensaje de error
+  return "No se encontró el elemento";
 };
 
 module.exports = buscarElemento;
